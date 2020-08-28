14119ae8d324895c36d35eb41affb9b3f065062f
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-28 08:33:04 -0700

    created a list of all the pizza toppings and added an object for pizza in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+// Business Logic
+function Pizza(toppings) {
+  this.toppings = toppings;
+}
+
+Pizza.prototype.price = function() {
+
+}
+
+// Business Pizza Logic
+
+
+
+// User Interface Logic
 $(document).ready(function() {
   
   
   event.preventDefault();
 });
+
