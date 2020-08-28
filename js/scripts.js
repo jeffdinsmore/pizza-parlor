7db5aaa2ada9b76e83d84746273c79e33068da3b
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-28 08:44:49 -0700

    adding veggie toppings function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // Business Logic
-function Pizza(toppings) {
+function Pizza(size, type, toppings) {
+  this.size = size;
+  this.type = type;
   this.toppings = toppings;
 }
 
@@ -7,9 +9,29 @@ Pizza.prototype.price = function() {
 
 }
 
-// Business Pizza Logic
+// Business Pizza Type Logic
+function PizzaType(small, medium, large, xlarge) {
+  this.small = small;
+  this.meduim = medium;
+  this.large = large;
+  this.xlarge = xlarge;
+}
 
-
+// Business Pizza Toppings Logic
+function PizzaToppings(olives, broccoli, spinach, jalapenos, tomatoes, sunDriedTomatoes, mushrooms, pineapple, onions, redPeppers, greenPeppers, pepperoncini) {
+  this.olives = olives;
+  this.broccoli = broccoli;
+  this.spinach = spinach;
+  this.jalapenos = jalapenos;
+  this.tomatoes = tomatoes;
+  this.sunDriedTomatoes = sunDriedTomatoes;
+  this.mushrooms = mushrooms;
+  this.pineapple = pineapple;
+  this. onions = onions;
+  this.redPeppers = redPeppers;
+  this.greenPeppers = greenPeppers;
+  this.pepperoncini = pepperoncini;
+}
 
 // User Interface Logic
 $(document).ready(function() {
